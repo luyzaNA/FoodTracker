dc7c14cfd135a598a22592d0b58ee89c82b4d391
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-03-22 23:05:41 +0200

    Add food functionality

---
+++ BE/migrations/Version20250322163505.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250322163505 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE food (name VARCHAR(255) NOT NULL, calories INT NOT NULL, proteins DOUBLE PRECISION NOT NULL, carbohydrates DOUBLE PRECISION NOT NULL, fats DOUBLE PRECISION NOT NULL, fiber DOUBLE PRECISION NOT NULL, PRIMARY KEY(name)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE food');
+    }
+}
